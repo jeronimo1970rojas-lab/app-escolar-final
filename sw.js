bad4b364903aab89ff955cf258dfd0eb3a27a71c
jeronimo1970rojas-lab <jeronimo1970rojas@gmail.com>
2026-04-24 22:18:06 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'app-escolar-v3';
+const CACHE_NAME = 'app-escolar-v4';
 
 const urlsToCache = [
   './',
   './index.html',
   './app.js',
   './style.css',
+  './manifest.json',
   './logo.png',
   './profesor.jpg',
   './icon-192.png',
@@ -12,10 +13,28 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
