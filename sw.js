f2215512d3f14062911462bf68c09889c59227cc
jeronimo1970rojas-lab <jeronimo1970rojas@gmail.com>
2026-04-30 22:33:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pre-promo-b-v1';
+const CACHE_NAME = 'app-escolar-v1';
 
 const urlsToCache = [
   './',
@@ -6,24 +6,14 @@ const urlsToCache = [
   './app.js',
   './style.css',
   './manifest.json',
-  './logo.png',
-  './profesor.jpg',
   './icon-192.png',
   './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    self.clients.claim()
   );
 });
 
@@ -31,26 +21,5 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-
-  event.waitUntil(
-    clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    }).then(clientList => {
-      for (const client of clientList) {
-        if ('focus' in client) {
-          return client.focus();
-        }
-      }
-
-      if (clients.openWindow) {
-        return clients.openWindow('./');
-      }
-    })
   );
 });
