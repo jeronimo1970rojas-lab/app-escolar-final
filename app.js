5b1f0128ad9afc75090f948f529df923dd1b5b2e
jeronimo1970rojas-lab <jeronimo1970rojas@gmail.com>
2026-05-05 18:14:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ var mainView = app.views.create('.view-main', {
   url: '/'
 });
 
-var url = 'TU_URL_DE_GOOGLE_APPS_SCRIPT';
+var url = 'https://script.google.com/macros/s/AKfycbw5qQ1LCdGap5ye3Bi9Ir45ep8KtGwSKc5BGVjPfsjjsFM7iorYmHUdC5_XqrV4kVM/exec';
 
 var datosGlobal = null;
 
